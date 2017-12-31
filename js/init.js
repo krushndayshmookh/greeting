26f2a3bb193d5453c5c33cd50f7dddc29f928ca5
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2017-12-31 21:38:39 +0530

    finished

--- js/init.js
+++ js/init.js
@@ -1,25 +1,7 @@
 $(document).ready(function () {
-
-	$('.button-collapse').sideNav();
-	$('.parallax').parallax();
-
-	$("#panel-0").slideUp("fast", addAnimationClass("#panel-back-1", "panel-back-animated"));
-	//$("#panel-back-1").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",$("#scrol-btn-1").addClass("animated"));
-
-
-
-
-
-
-}); // end of document ready
-
-
-
-
-
-
-
-
-function addAnimationClass(element, animation) {
-	$(element).addClass(animation);
-}
+    AOS.init({
+      duration: 600,
+      //easing: 'ease-in-sine',
+      delay: 300,
+    });
+});
